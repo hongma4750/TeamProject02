3f20acdde5b6ffa40985263daf22d832895ff3bb
aaaa <aaaa@sist00>
2016-07-27 18:09:16 +0900

    간단한 Review <list,detail,write,writeAF>

---
+++ TeamProject02ver0.1/WebContent/SNS/ReviewDetail.jsp
@@ -0,0 +1,73 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="sist.co.Member.MemberDTO" %>
+     <%@ page import="sist.co.Review.ReviewDAO" %>
+     <%@ page import="sist.co.Review.ReviewDTO" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>리뷰 디테일</title>
+
+<script  src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+<%
+MemberDTO memberdto = (MemberDTO)session.getAttribute("login");
+%>
+
+<h3><%=memberdto.getM_id() %>님 하이요</h3>
+<hr>
+<%
+
+String r_seqs = request.getParameter("r_seq");
+int r_seq = Integer.parseInt(r_seqs);
+ReviewDAO redao = ReviewDAO.getInstance();
+
+redao.addReadCount(r_seq);
+ReviewDTO redto = redao.getReviewDTO(r_seq);
+
+%>
+<div id="middle_wrap">
+
+	<table class="content_table">
+		<tr>
+			<td> <%=redto.getM_id() %> 님 작성글</td>
+		</tr>
+		
+		<tr>
+			<td><%=redto.getR_title() %>&nbsp;&nbsp;&nbsp;&nbsp;조회:<%=redto.getR_readcount() %>&nbsp;
+				좋아요:<input type="text" id ="like" value="<%=redto.getR_like() %>" size="2" readonly> 
+				<input type="button" value="좋아용" id="addlike">
+			</td>
+		</tr>
+		
+		<tr>
+			<td><textarea rows="20" cols="50" readonly ><%=redto.getR_content()%></textarea></td>
+		</tr>
+		
+	</table>
+	<input type="button" value="돌아가기" onclick="javascript:location.href='ReviewList.jsp'">
+</div>
+<hr>
+
+<script>
+	var like = <%=redto.getR_like()%>;
+	
+    $('#addlike').click(function(){
+        $.ajax({
+            url:'addLike.jsp?r_seq=<%=redto.getR_seq()%>' ,
+            success:function(data){
+            },complete : function(data) {
+            	like +=1;
+            	$('#like').val(like);
+          }
+            
+        });
+    });
+</script>
+</body>
+</html>
---
+++ TeamProject02ver0.1/src/sist/co/Review/ReviewDAO.java
@@ -0,0 +1,204 @@
+package sist.co.Review;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import sist.co.DBManager.DBManager;
+
+public class ReviewDAO {
+
+	
+	private static ReviewDAO reviewDao;
+	
+	
+	public static ReviewDAO getInstance(){
+		if (reviewDao == null) {
+			reviewDao = new ReviewDAO();
+		}
+		return reviewDao;
+	}
+	
+	
+	//review 게시판 처음에 보여주기 위해 List<ReviewDTO> 형태로 리턴함
+	public List<ReviewDTO> selectAllReview(){
+		
+		List<ReviewDTO> list = new ArrayList<ReviewDTO>();
+		
+		String sql = "select * from review order by r_seq desc";
+		
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		
+		try {
+			conn=DBManager.getConnection();
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()){
+				ReviewDTO redto = new ReviewDTO();
+				int i = 1;
+				redto.setR_seq(rs.getInt(i++));
+				redto.setM_id(rs.getString(i++));
+				redto.setR_title(rs.getString(i++));
+				redto.setR_content(rs.getString(i++));
+				redto.setR_writedate(rs.getTimestamp(i++));
+				redto.setR_like(rs.getInt(i++));
+				redto.setR_readcount(rs.getInt(i++));
+				
+				list.add(redto);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally{
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		return list;
+	}
+	
+	//review 게시판 글 작성 할 때 사용됨
+	public int addReview(ReviewDTO redto){
+		int result = -1;
+		
+		String sql ="insert into review values (review_seq.nextval,?,?,?,sysdate,0,0)";
+		
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		
+		
+		try {
+			conn=DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, redto.getM_id());
+			pstmt.setString(2, redto.getR_title());
+			pstmt.setString(3, redto.getR_content());
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally{
+			DBManager.close(conn, pstmt);
+		}
+		
+		
+		return result;
+	}
+
+	//review 게시판 detail에 사용됨
+	public ReviewDTO getReviewDTO(int r_seq){
+		ReviewDTO redto = null;
+		
+		String sql = "select * from review where r_seq=?";
+		
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1,r_seq);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()){
+				redto = new ReviewDTO();
+				int i = 1;
+				
+				redto.setR_seq(rs.getInt(i++));
+				redto.setM_id(rs.getString(i++));
+				redto.setR_title(rs.getString(i++));
+				redto.setR_content(rs.getString(i++));
+				redto.setR_writedate(rs.getTimestamp(i++));
+				redto.setR_like(rs.getInt(i++));
+				redto.setR_readcount(rs.getInt(i++));
+				
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally{
+			DBManager.close(conn, pstmt, rs);
+		}
+		
+		
+		return redto;
+	}
+
+	//review 게시판 detail 눌렀을시 readcount 증가용 메서드
+	public void addReadCount(int r_seq){
+		String sql = "update review set r_readcount = r_readcount+1 where r_seq = ?";
+		
+		int result = 0;
+		
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		
+		
+		try {
+			conn = DBManager.getConnection();
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setInt(1,r_seq);
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally{
+			DBManager.close(conn, pstmt);
+		}
+	}
+	
+	//review 게시판 detail 안에서 좋아요 누르면 증가
+	public void addLike(int r_seq){
+		String sql = "update review set r_like = r_like+1 where r_seq = ?";
+		
+		int result = 0;
+		
+		Connection conn=null;
+		PreparedStatement pstmt = null;
+		
+		
+		try {
+			conn = DBManager.getConnection();
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setInt(1,r_seq);
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally{
+			DBManager.close(conn, pstmt);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
